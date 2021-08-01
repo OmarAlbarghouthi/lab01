135b0693e89a575b1ef71910a0455efb30b10302
OmarAlbarghouthi <omar.qb8077@gmail.com>
2021-08-01 16:59:01 +0300

    created a new files

---
+++ js/web.js
@@ -0,0 +1,10 @@
+let userName = prompt('Please enter your first name');
+alert('Hello ' + userName + ',  hope you enjoy our website ');
+document.write('Welcome' + userName);
+
+let userAge = Number (prompt('How old are you?'));
+
+userAge += 10;
+ console.log(userAge);
+
+
